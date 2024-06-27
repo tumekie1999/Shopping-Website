e37ee552185f24e726534314de486793985f3a58
tumekie1999 <tb000223@sbuniv.edu>
2024-06-27 14:03:11 -0500

    added ja products and fixed styling

--- src/products.js
+++ src/products.js
@@ -1,59 +1,66 @@
-import product1 from "./assets/products/1.png";
-import product2 from "./assets/products/2.png";
-import product3 from "./assets/products/3.png";
-import product4 from "./assets/products/4.png";
-import product5 from "./assets/products/5.png";
-import product6 from "./assets/products/6.webp";
-import product7 from "./assets/products/7.webp";
-import product8 from "./assets/products/8.webp";
+import product1 from "./assets/products/browning.jpeg";
+import product2 from "./assets/products/oats.jpeg";
+import product3 from "./assets/products/cream.jpeg";
+import product4 from "./assets/products/callaloo.jpeg";
+import product5 from "./assets/products/crackers.jpeg";
+import product6 from "./assets/products/serge.jpeg";
+import product7 from "./assets/products/jerk.jpeg";
+import product8 from "./assets/products/curry.jpeg";
+import product9 from "./assets/products/coffee.jpeg";
 
 export const PRODUCTS = [
   {
     id: 1,
-    productName: "IPhone",
+    productName: "Browning",
     price: 999.0,
     productImage: product1,
   },
   {
     id: 2,
-    productName: "Macbook Pro 2022 (M1)",
+    productName: "Oats",
     price: 1999.0,
     productImage: product2,
   },
   {
     id: 3,
-    productName: "Cannon M50 Camera",
+    productName: "Cream Crackers",
     price: 699.0,
     productImage: product3,
   },
   {
     id: 4,
-    productName: "WLS Van Gogh Denim Jacket",
+    productName: "Callaloo",
     price: 228.0,
     productImage: product4,
   },
   {
     id: 5,
-    productName: "LED Light Strips",
+    productName: "Water Crackers",
     price: 19.99,
     productImage: product5,
   },
   {
     id: 6,
-    productName: "SPECTRUM LS TEE",
+    productName: "Serge Box Milk",
     price: 68.0,
     productImage: product6,
   },
   {
     id: 7,
-    productName: "AUTO SERVICE SHIRT by GOLF WANG",
+    productName: "Jerk Seasoning",
     price: 120.0,
     productImage: product7,
   },
   {
     id: 8,
-    productName: "DON'T TRIP UNSTRUCTURED HAT",
+    productName: "Curry",
     price: 40.0,
     productImage: product8,
+  },
+  {
+    id: 9,
+    productName: "Coffee",
+    price: 40.0,
+    productImage: product9,
   },
 ];
